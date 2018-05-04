aca68dab8252f098fafae6402c1fdf42af362868
unknown <cmca>
2018-05-04 16:38:16 +0800

    0504

--- PD2/hw/hw4/hw4.c
+++ PD2/hw/hw4/hw4.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#define Maxkey 20
-#define Maxline 20
+#define Maxkey 100
 
 struct tnode{
 	char key[Maxkey];
-	struct tnode *next;
+	int cnt;
+	struct tnode *link;
 };
 
 void printlist(struct tnode* L)
@@ -14,32 +14,102 @@ void printlist(struct tnode* L)
 	struct tnode *lptr = L ;
 	while(lptr)
 	{
-		printf("%s\n",lptr->key);
+		printf("%d\t%s\n",lptr->cnt,lptr->key);
+		lptr =lptr ->link;
 	}
-
 }
 
-struct tnode* insert(struct tnode* L , char* key)
+struct tnode* newnode(char key[Maxkey])
 {
-	struct tnode* tmp = (struct tnode*)malloc(sizeof(struct tnode));
+	struct tnode* tmp =(struct tnode*)malloc(sizeof(struct tnode));
 	strcpy(tmp->key,key);
-	if(L == NULL)
-	{
-		tmp->next = NULL;
-		return tmp;
-	}
-	tmp -> next = L;
+	tmp->cnt=1;
+	tmp->link=NULL;
 	return tmp;
+}
+
+struct tnode* insert(struct tnode* node , char key[Maxkey])
+{
+	if(node == NULL) return newnode(key);
+	if(strcmp(node->key,key)==0)node->cnt++;
+	else node->link = insert(node->link,key);
+	return node;
+}
+
+int getword(char *x,int max)
+{
+	int i=0,c,flag=0;
+	while((c=getchar())!=EOF)
+	{
+		if(i<max && ((c>=65&&c<=90)||(c>=97&&c<=122)))
+		{	
+			flag=1;
+			x[i]=c;
+			i++;
+		}
+		else if(flag) break;
+	}
+	x[i]='\0';
+	return i;
+}
+
+void swap(struct tnode *a,struct tnode *b)
+{
+	char tmpk[Maxkey];
+	int tmpcnt;
+
+	strcpy(tmpk,a->key);
+	tmpcnt = a->cnt;
+	strcpy(a->key,b->key);
+	a->cnt = b->cnt;
+	strcpy(b->key,tmpk);
+	b->cnt = tmpcnt;
+}
+
+void sort1(struct tnode *L)	//bubblesort 
+{
+	int swapped;
+	struct tnode *pt1,*pt2=NULL;
+
+	if(L==NULL)return;
+
+	do{
+		swapped=0;
+		pt1=L;
+		while(pt1->link != pt2)
+		{
+			if(pt1->cnt < pt1->link->cnt)
+			{
+				swap(pt1,pt1->link);
+				swapped=1;
+			}
+			pt1=pt1->link;
+		}
+		pt2=pt1;
+
+	}while(swapped);
 
 }
+
+// void sort2(struct tnode * L)	//sorting key value if same cnt
+// {
+// 	struct tnode *t=L;
+// 	while(t)
+// 	{
+
+// 	}
+// }
 
 int main(){
 
-	struct tnode *L;
-	char line[Maxline];
-	while(fgets(line,Maxline,stdin))
+	struct tnode *linklist=NULL;
+	char key[Maxkey];
+	while(getword(key,Maxkey))
 	{
-		L = insert(L,line);
+		linklist = insert(linklist,key);
 	}
-	printlist(L);
+	sort1(linklist);
+	// sort2(linklist);
+	printlist(linklist);
+
 return 0;}
